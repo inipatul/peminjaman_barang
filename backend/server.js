470b97a48684da7b8969fe4281959237fe343669
inipatul <inipatul4@gmail.com>
2026-06-11 19:44:31 +0700

    connect railway mysql

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  PORT: process.env.MYSQLPORT || 3306,
 });
 
 db.connect((err) => {
