5c26419c95148bdd194f8fe5fbf8dc425b529ab1
inipatul <inipatul4@gmail.com>
2026-06-12 08:15:09 +0700

    remove env from git

--- backend/server.js
+++ backend/server.js
@@ -14,21 +14,26 @@ app.use(express.json());
 // Frontend
 app.use(express.static(path.join(__dirname, "../frontend")));
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("[ERROR] Database:", err.message);
     process.exit(1);
   }
 
   console.log("[OK] Database terhubung");
+  connection.release();
 });
 
 // =======================
